345453b261aae1c4f812a0a62b6afae8ba63763d
hawn1753 <john175258@gmail.com>
2020-07-11 22:48:03 +0900

    Make XAQuery

--- stocklab/agent/ebest.py
+++ stocklab/agent/ebest.py
@@ -1,6 +1,8 @@
 import configparser
 import win32com.client
 import pythoncom
+from datetime import datetime
+import time
 
 class XASession:
     # 로그인 상태를 확인하기 위한 클래스 변수
@@ -27,6 +29,11 @@ class XASession:
 
 
 class EBest:
+
+    # xing API TR호출 제약조건
+    QUERY_LIMIT_10MIN = 200
+    LIMIT_SECONDS = 600 # 10min
+
     def __init__(self, mode=None):
         """
         config.ini 파일을 로드해 사용자, 서버 정보 저장
@@ -52,6 +59,11 @@ class EBest:
 
         self.xa_session_client = win32com.client.DispatchWithEvents("XA_Session.XASession",XASession)
 
+        # 현재 리스트 갯수
+        self.query_cnt = []
+
+
+
     def login(self):
         self.xa_session_client.ConnectServer(self.host, self.host)
         self.xa_session_client.Login(self.user, self.passwd, self.cert_passwd, 0, 0)
@@ -64,3 +76,82 @@ class EBest:
         XASession.login_state =0
         self.xa_session_client.DisconnectServer()
 
+    def _excute_query(self, res, in_block_name, out_block_name, *out_fields, **set_fields):
+        """
+        TR 코드를 실행하기 위한 메서드
+        :param res:str 리소스 이름(TR)
+        :param in_block_name:str 인 블록 이름
+        :param out_block_name:str 아웃 블록 이름
+        :param out_params:list 출력 필드 리스트
+        :param in_farams:dict 인 블록에 설정할 필드 딕셔너리
+        :return result:list 결과를 list에 담아 반환
+        """
+
+        time.sleep(1)
+        print("current query cnt:", len(self.query_cnt))
+        print(res, in_block_name, out_block_name)
+        while len(self.query_cnt) >= EBest.QUERY_LIMIT_10MIN:
+            time.sleep(1)
+            print("waiting for execute query... current query cht:", len(self.query_cnt))
+            self.query_cnt = list(filter(lambda x: (datetime.today() - x).total_seconds() < EBest.LIMIT_SECONDS, self.query_cnt))
+
+        xa_query = win32com.client.DispatchWithEvents("XA_DataSet.XAQuery", XAQuery)
+        xa_query.LoadFromResFile(XAQuery.RES_PATH + res + ".res")
+        # in_block_name 설정
+        for key, value in set_fields.items():
+            xa_query.SetFieldData(in_block_name, key, 0, value)
+        errorCode = xa_query.Request(0)
+
+        # 요청 후 대기
+        waiting_cnt = 0
+        while xa_query.tr_run_state == 0:
+            waiting_cnt += 1
+            if waiting_cnt % 100000 == 0:
+                print("Waiting....", self.xa_session_client.GetLastError())
+            pythoncom.PumpWaitingMessages()
+
+        # 결과 블록
+        result = []
+        count = xa_query.GetBlockCount(out_block_name)
+
+        for i in range(count):
+            item = {}
+            for field in out_fields:
+                value = xa_query.GetFieldData(out_block_name, field, i)
+                item[value] = value
+            result.append(item)
+
+        # 제약시간 체크
+        XAQuery.tr_run_state = 0
+        self.query_cnt.append(datetime.today())
+
+        # 영문 필드명을 한글 필드명으로 변환
+        for item in result:
+            for field in list(item.keys()):
+                if getattr(Field, res, None):
+                    res_field = getattr(Field, res, None)
+                    if out_block_name in res_field:
+                        field_hname = res_field[out_block_name]
+                        if field in field_hname:
+                            item[field_hname[field]] = item[field]
+                            item.pop(field)
+
+        return result
+
+
+
+
+class XAQuery:
+    # xingAPI에서 TR을 내려받은 폴더
+    RES_PATH = "C:/eBEST/xingAPI/Res"
+    # OnReceiveData에서 Data를 받으면 1로 바뀜
+    tr_run_state = 0
+
+    # 데이터가 수신되면 tr_run_state를 1로 설정
+    def OnReceiveData(self, code):
+        print("OnReceiveData", code)
+        XAQuery.tr_run_state = 1
+
+    # 요청한 API에 대해 메시지를 수신했을 때 발생하는 이벤트
+    def OnReceiveMessage(self, error, code, message):
+        print("OnreceiveMessage", error, code, message)
